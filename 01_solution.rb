1810452c4fa8ce74e03d67a37d9a0fb390e1deb1
Katie Lin <katielin019@gmail.com>
2024-09-16 20:53:05 -0700

    day 1, part 1 solution added

--- 01_solution.rb
+++ 01_solution.rb
@@ -1,4 +1,16 @@
-input = File.read("01_input.txt").split
-(0..5).each do |i|
-	puts input[i]
+require 'set'
+
+def part1(target)
+	file = File.open("01_input.txt")
+	input = file.readlines.map(&:to_i)
+	set = Set.new(input)
+	# p set	
+	input.each do |x|
+		if set.add?(target - x) == nil
+			# return "#{x}, #{target - x}"
+			return x * (target - x)
+		end
+	end
 end
+
+p part1(2020)
