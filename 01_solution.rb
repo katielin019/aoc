df0c2bbb832f07a3fcbf1abfa1532cdec35d3cfc
Katie Lin <katielin019@gmail.com>
2024-09-16 21:20:56 -0700

    add completed ruby solution

--- 01_solution.rb
+++ 01_solution.rb
@@ -1,16 +1,27 @@
 require 'set'
 
-def part1(target)
-	file = File.open("01_input.txt")
-	input = file.readlines.map(&:to_i)
-	set = Set.new(input)
-	# p set	
-	input.each do |x|
-		if set.add?(target - x) == nil
+def part1(entries, set, target)
+	entries.each do |x|
+		if set.include?(target - x)
 			# return "#{x}, #{target - x}"
 			return x * (target - x)
 		end
 	end
 end
 
-p part1(2020)
+def part2(entries, set, target)
+	entries.each do |x|
+		entries.each do |y|
+			if set.include?(target - x - y)
+				return x * y * (target - x - y)
+			end
+		end
+	end
+end
+
+file = File.open("01_input.txt")
+input = file.readlines.map(&:to_i)
+set = Set.new(input)
+
+p part1(input, set, 2020)
+p part2(input, set, 2020)
