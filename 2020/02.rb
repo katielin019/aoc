6ce05325fab62ed92e00ed884c8c7684254e5762
Katie Lin <katielin019@gmail.com>
2024-12-08 14:07:04 -0800

    get working directory to read input

--- 2020/02.rb
+++ 2020/02.rb
@@ -2,9 +2,10 @@ def part1()
 
 end
 
-file = File.new('input/02 copy.txt')
-input = file.readlines.map()
+context = Dir.getwd.split('/').last
+p context
+# Dir.getwd == "aoc" ? file = File.new('/2020/input/02.txt') : file = File.new('input/02.txt')
+# file = File.new('input/02.txt')
+# input = file.readlines
 
-p input
-
-#OUTPUT: #<Enumerator: ["2-6 w: wkwwwfwwpvw\n", "14-15 v: hvhvlhvvvwxvdvscdpvg\n", "2-3 b: bkkb\n", "3-4 v: wgvvzvhcvlrvjvvmv\n", "11-12 c: cccccccccqcccckcc"]:map>
+# p input
