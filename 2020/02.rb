b1298538b23481a64a880253846045f42f9a90ac
Katie Lin <katielin019@gmail.com>
2024-12-08 15:36:48 -0800

    wrote a janky solution to part 2; realized that i offset the index in the wrong direction which is why my solution didn't work

--- 2020/02.rb
+++ 2020/02.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def part1(input)
   valid = 0
   input.each do |x|         # 1-3 a: abcde
@@ -14,10 +16,37 @@ def part1(input)
   valid
 end
 
+def part2(input)
+  valid = 0
+  input.each do |x|
+    valid += 1 if helper(x.split(' '))
+  end
+  valid
+end
+
+def helper(args)
+  # ["1-3", "a:", "abcde"]
+  p args
+  positions = args[0].split('-').map(&:to_i)
+  p positions
+  letter = args[1][0]
+  str = args.last
+  chars = positions.map{|p| str[p-1]}
+  # chars = Set.new(positions.map{|p| str[p+1]})
+  p chars
+  chars = Set.new(chars)
+  # chars.size() == 1 ? (chars.include?(letter) ? return false : return false) : c
+  # chars.size() == 2 && chars.include?(letter) ? return true : return false
+  return true if chars.size() == 2 && chars.include?(letter)
+  return false
+end
+
 
 day = "02"
 context = Dir.getwd.split('/').last
-context == "aoc" ? file = File.new("2020/input/#{day}.txt") : file = File.new("input/#{day}.txt")
+# context == "aoc" ? file = File.new("2020/input/#{day}.txt") : file = File.new("input/#{day}.txt")
+context == "aoc" ? file = File.new("2020/input/#{day} copy.txt") : file = File.new("input/#{day} copy.txt")
 input = file.readlines
 
-p part1(input)
+# p part1(input)
+p part2(input)
