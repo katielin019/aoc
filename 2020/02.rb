8fe0a641b5c1caaa9c6fc24d0c954222a10a88d4
Katie Lin <katielin019@gmail.com>
2024-12-08 13:48:21 -0800

    testing something

---
+++ 2020/02.rb
@@ -0,0 +1,10 @@
+def part1()
+
+end
+
+file = File.new('input/02 copy.txt')
+input = file.readlines.map()
+
+p input
+
+#OUTPUT: #<Enumerator: ["2-6 w: wkwwwfwwpvw\n", "14-15 v: hvhvlhvvvwxvdvscdpvg\n", "2-3 b: bkkb\n", "3-4 v: wgvvzvhcvlrvjvvmv\n", "11-12 c: cccccccccqcccckcc"]:map>
