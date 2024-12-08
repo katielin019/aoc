3c43e9d6a30635e450c4f7b79b8e9ca5e42504af
Katie Lin <katielin019@gmail.com>
2024-12-08 14:38:04 -0800

    debugging

--- 2020/02.rb
+++ 2020/02.rb
@@ -1,20 +1,26 @@
 def part1(input)
   count = 0;
+  bad = 0;
   input.each do |x|
-    # 2-6 w: wkwwwfwwpvw
     arr = x.split(' ')
-    # ["2-6", "w:", "wkwwwfwwpvw"]
-    # /a{1,3}/
-    letter = arr[1][0]
-    range = arr.first.split('-')
-    lower = range.first
-    upper = range.last
-     if arr.last.match?(/#{letter}{#{lower},#{upper}}/)
-      count += 1
-     end
+    pattern = helper(arr)
+    arr.last.match?(/#{pattern}/) ? count += 1 : bad += 1
   end
-  return count
+  p count, bad #=> 357\n643\n[357, 643]
 end
+
+def helper(arr)
+  letter = arr[1][0]
+  range = arr.first.split('-')
+  lower = range.first
+  upper = range.last
+  # if arr.last.match?(/#{letter}{#{lower},#{upper}}/)
+  #   count += 1
+  #   p "/#{letter}{#{lower},#{upper}}/"
+  # end
+  return "#{letter}{#{lower},#{upper}}"
+end
+
 
 day = "02"
 context = Dir.getwd.split('/').last
