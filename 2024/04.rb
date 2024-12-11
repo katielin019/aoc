add1c31e3725301fa17d905d849a367f60ad8a39
Katie Lin <katielin019@gmail.com>
2024-12-10 18:28:55 -0800

    start adding search directions

--- 2024/04.rb
+++ 2024/04.rb
@@ -1,13 +1,63 @@
-def part_1(input)
-  grid = Array.new(input.length, Array.new(input[0].length))
-  locs = []
-  (0...input.length).each do |row|
-    tmp = input[row].chars
-    # ["A", "B", "C", "D", "E"]
-    grid[row] = tmp.flat_map.with_index { |ch, col| ch == 'X' ?  locs.push([row, col]) && [ch] : [ch]}
+class Grid
+  attr_reader :grid, :locs, :count
+  attr_writer :count
+  def initialize(input)
+    @grid = Array.new(input.length, Array.new(input[0].length))
+    @locs = Array.new()
+    @count = 0
+    (0...input.length).each do |row|
+      tmp = input[row].chars
+      @grid[row] = tmp.flat_map.with_index { |c, col| c == 'X' ? locs.push([row, col]) && [c] : [c] }
+    end
   end
-  p grid
-  locs
+
+  # def search_all(r, c)
+  #   total = 0
+  #   total +=
+  #   return 0
+  # end
+
+  def search_N(r, c)
+    return false if (r - 3 < 0)
+    return @grid[r-1][c] == 'M' && @grid[r-2][c] == 'A' && @grid[r-3][c] == 'S'
+  end
+
+  def search_NE(r, c)
+
+  end
+
+  def search_E(r, c)
+    return false if ()
+  end
+
+  def search_SE(r, c)
+  end
+
+  def search_S(r, c)
+  end
+
+  def search_SW(r, c)
+  end
+
+  def search_W(r, c)
+    return false if (c - 3 < 0)
+    return @grid[r][c-1] == 'M' && @grid[r][c-2] == 'A' && @grid[r][c-3] == 'S'
+  end
+
+  def search_NW(r, c)
+    return false if (c - 3 < 0) || (r - 3 < 0)
+    return @grid[r-1][c-1] == 'M' && @grid[r-2][c-2] == 'A' && @grid[r-3][c-3] == 'S'
+  end
+end
+
+def part_1(input)
+  grid = Grid.new(input)
+  grid.locs.each do |loc|
+    # grid.count += grid.search_all(loc.first, loc.last)
+    grid.count += 1 if grid.search_N(loc.first, loc.last)
+    grid.count += 1 if grid.search_NE(loc.first, loc.last)
+  end
+  grid.count
 end
 
 sample_1 = ["MMMSXXMASM",
