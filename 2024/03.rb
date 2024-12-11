31712959c34c3af89cf39f457b37986ad82ded6f
Katie Lin <katielin019@gmail.com>
2024-12-10 16:47:01 -0800

    add partial sol

--- 2024/03.rb
+++ 2024/03.rb
@@ -1,5 +1,30 @@
+# str.to_enum(:scan, regex).map { Regexp.last_match }
+# source: https://brettterpstra.com/2023/12/17/ruby-regexp-scan-with-matchdata/
+
+# regexp: /mul\(\d{1,3},\d{1,3}\)/
+
 def part_1(input)
+  total = 0
+  input.each do |x|
+    matches = x.scan(/mul\(\d{1,3},\d{1,3}\)/)
+    matches.each do |x|
+      total += compute(x)
+    end
+    # matches.map { |x| compute(x).to_i }
+    # total += matches.inject(0, :+)  # returns 0 (instead of nil) if array is empty
+  end
+  total
 end
+
+def compute(str)
+  # "mul(" = 0-3
+  s = str[4..-1].chop!
+  vals = s.split(",").map(&:to_i)
+  return vals.first * vals.last
+end
+
+str = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
+p part_1(str)
 
 def part_2(input)
 end
@@ -19,4 +44,4 @@ def main()
   puts "Elapsed time: #{elapsed}"
 end
 
-main()
+# main()
