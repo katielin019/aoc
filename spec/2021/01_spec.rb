26a7baccb6356de9e3e13168bc87d99c6a6505b9
Katie Lin <katielin019@gmail.com>
2024-12-11 13:21:57 -0800

    fixed input retrieval

---
+++ spec/2021/01_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+RSpec.describe "Day 1: Challenge Title" do
+  let(:runner) { Runner.new("2021/01") }
+  let(:input) do
+    <<~TXT
+    TXT
+  end
+
+  describe "Part One" do
+    let(:solution) { runner.execute!(input, part: 1) }
+  end
+
+  describe "Part Two" do
+    let(:solution) { runner.execute!(input, part: 2) }
+  end
+end
