a0f9c5f0b1782aff63d3007dcb4f85185ced14e9
Katie Lin <katielin019@gmail.com>
2024-12-12 12:26:51 -0800

    solution for part 1 works on sample input

--- lib/2024/05.rb
+++ lib/2024/05.rb
@@ -31,32 +31,51 @@ INPUT = <<~TEXT
       97,13,75,29,47
       TEXT
 
-SECTIONS = INPUT.split("\n\n")
-RULES = SECTIONS.first.split
-ORD = RULES.map { |rule| rule.split("|") }
-
-hash = {}
-
-LEFT = ORD.map(&:first).to_set
-LEFT.each do |page|
-  hash[page.to_i] = ORD.select { |rule| rule.first == page }.map(&:last).map(&:to_i)
-end
-
 # puts hash
 
 # {"47"=>[["47", "53"], ["47", "13"], ["47", "61"], ["47", "29"]], "97"=>[["97", "13"], ["97", "61"], ["97", "47"], ["97", "29"], ["97", "53"], ["97", "75"]], "75"=>[["75", "29"], ["75", "53"], ["75", "47"], ["75", "61"], ["75", "13"]], "61"=>[["61", "13"], ["61", "53"], ["61", "29"]], "29"=>[["29", "13"]], "53"=>[["53", "29"], ["53", "13"]]}
 # {47=>[53, 13, 61, 29], 97=>[13, 61, 47, 29, 53, 75], 75=>[29, 53, 47, 61, 13], 61=>[13, 53, 29], 29=>[13], 53=>[29, 13]}
 
+SECTIONS = INPUT.split("\n\n")
+RULES = SECTIONS.first.split
+ORD = RULES.map { |rule| rule.split("|") }
+
+HASH = {}
+
+LEFT = ORD.map(&:first).to_set
+LEFT.each do |page|
+  HASH[page.to_i] = ORD.select { |rule| rule.first == page }.map(&:last).map(&:to_i)
+end
+
 UPDATE_LIST = SECTIONS.last.split
 UPDATES = UPDATE_LIST.map { |u| u.split(",") }
-# total = 0
 
-UPDATES.each do |u|
-  u.map!(&:to_i)
-  u.each do |page|
-    p page, hash.values_at(page).flatten
+def scanner(arr)
+  violations = HASH.values_at(arr.first).flatten
+  arr.shift
+  if arr.empty?
+    return true
+  elsif violations.empty?
+    return scanner(arr)
+  elsif violations.intersect?(arr)
+    return false
+  else
+    return scanner(arr)
   end
 end
+
+def main()
+  total = 0
+  UPDATES.each do |update|
+    # p update if scanner(update.map!(&:to_i).reverse)
+    total += update[update.length/2] if scanner(update.map!(&:to_i).reverse)
+  end
+  p total
+end
+
+main()
+
+# solve!("part 1", main())
 
 ## APPROACH ?? :: 61,13,29 => 29,13,61 && 29|13
 # 29,61,13 (not directly next to each other)
