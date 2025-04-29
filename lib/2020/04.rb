f56b67aa181152fbdf312da2b16414843fd22fef
Katie Lin <katielin019@gmail.com>
2025-04-28 22:26:31 -0700

    add skeleton for day 4

---
+++ lib/2020/04.rb
@@ -0,0 +1,18 @@
+# REQUIRED FIELDS
+# byr (Birth Year)
+# iyr (Issue Year)
+# eyr (Expiration Year)
+# hgt (Height)
+# hcl (Hair Color)
+# ecl (Eye Color)
+# pid (Passport ID)
+
+# OPTIONAL FIELD
+# cid (Country ID)
+
+REQUIRED = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+PASSPORTS = INPUT.split("\n\n")
+
+def part_1(input)
+
+end
--- spec/2020/04_spec.rb
+++ spec/2020/04_spec.rb
@@ -1,14 +1,31 @@
 require "spec_helper"
 
-RSpec.describe "Day 4: REPLACE_WITH_TITLE" do
+RSpec.describe "Day 4: Passport Processing" do
   let(:runner) { Runner.new("2020/04") }
   let(:input) do
     <<~TXT
+    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+    byr:1937 iyr:2017 cid:147 hgt:183cm
+
+    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+    hcl:#cfa07d byr:1929
+
+    hcl:#ae17e1 iyr:2013
+    eyr:2024
+    ecl:brn pid:760753108 byr:1931
+    hgt:179cm
+
+    hcl:#cfa07d eyr:2025 pid:166559648
+    iyr:2011 ecl:brn hgt:59in
     TXT
   end
 
   describe "(Part 1)" do
     let(:solution) { runner.execute!(input, part: 1) }
+
+    it "counts the number of valid passports" do
+      expect(solution).to eq(2)
+    end
   end
 
   describe "(Part 2)" do
