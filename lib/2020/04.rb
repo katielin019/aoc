4d55a6a43a7889896e8c2b44f6a4ee6f330abb83
Katie Lin <katielin019@gmail.com>
2025-04-29 21:26:03 -0700

    add rules, spec input for day 4 part 2

--- lib/2020/04.rb
+++ lib/2020/04.rb
@@ -22,3 +22,14 @@ def part_1(input, fields)
 end
 
 solve!("Part 1:", part_1(PASSPORTS, REQUIRED))
+
+RULES = {"byr": [1920, 2002],
+         "iyr": [2010, 2020],
+         "eyr": [2020, 2030],
+         "hgt": {"cm": [150, 193], "in": [59, 76]},
+         "hcl": ["#000000", "#ffffff"],
+         "ecl": ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+         "pid": [000_000_000, 999_999_999]}
+
+def part_2(input)
+end
--- spec/2020/04_spec.rb
+++ spec/2020/04_spec.rb
@@ -20,6 +20,37 @@ RSpec.describe "Day 4: Passport Processing" do
     TXT
   end
 
+  let(:strict_input) do
+    <<~TXT
+	eyr:1972 cid:100
+	hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+    
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+
+	iyr:2019
+	hcl:#602927 eyr:1967 hgt:170cm
+	ecl:grn pid:012533040 byr:1946
+
+	hgt:59cm ecl:zzz
+	eyr:2038 hcl:74454a iyr:2023
+	pid:3556412378 byr:2007
+    
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+
+	hcl:dab227 iyr:2012
+	ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+    TXT
+  end
+
   describe "(Part 1)" do
     let(:solution) { runner.execute!(input, part: 1) }
 
@@ -29,6 +60,10 @@ RSpec.describe "Day 4: Passport Processing" do
   end
 
   describe "(Part 2)" do
-    let(:solution) { runner.execute!(input, part: 2) }
+    let(:solution) { runner.execute!(strict_input, part: 2) }
+
+    it "counts the number of valid passports" do
+      expect(solution).to eq(4)
+    end
   end
 end
