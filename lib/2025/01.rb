168321eb96df721b91921339df7cfdb19920ba86
Katie Lin <katielin019@gmail.com>
2025-12-09 13:33:30 -0500

    answer too low

--- lib/2025/01.rb
+++ lib/2025/01.rb
@@ -28,9 +28,7 @@ def part_2(input)
     end
     result = dial + (direction * distance)
     dial = result % LENGTH
-    if dial == 0
-      password += 1
-    elsif result / 100 != 0
+    if result / 100 != 0
       password += 1
     end
   end
@@ -38,22 +36,3 @@ def part_2(input)
 end
 
 solve!("Part 2:", part_2(ROTATIONS))
-
-# 50 - 68 = -18 % 100 = 82 => -18 / 100 = -1
-# 82 - 30 = 52 => 52 / 100 = 0
-# 52 + 48 = 100 % 100 = 0 => 100 / 100 = 1
-# 0 - 5 = -5 % 100 = 95 => 95 / 100 = 0
-# 95 + 60 = 155 % 100 = 55 => 155 / 100 = 1
-# 55 - 55 = 0 % 100 = 0 => 0 / 100 = 0
-# 0 - 1 = -1 % 100 = 99 => -1 / 100 = -1
-# 99 - 99 = 0
-# 0 + 14 = 14 => 14 / 100 = 0
-# 14 - 82 = -68 % 100 = 32 => -68 / 100 = -1
-#
-# if dial == 0 exactly, incr. by one
-# otherwise, check if (dial + direction * distance) / 100
-#
-#
-# WAIT ... if dial pointing at 50, R1000 => + 10 times
-# if R100 with 50, +1 password, distance = 0
-# if L100 with 50, +1 password
