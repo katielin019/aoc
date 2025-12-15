cf3c05756a4ba7ee782d7cfe289a5cfdaa52898e
Katie Lin <katielin019@gmail.com>
2025-12-15 16:54:12 -0500

    drafting helper methods

--- lib/2025/02.rb
+++ lib/2025/02.rb
@@ -11,43 +11,68 @@ RANGES = INPUT.split(",").map do |range|
   range.match(RANGE_MATCHER).captures
 end
 
-#
+# Sums the invalid ids for all ranges in the puzzle input.
+# @param input {RANGES}
 def part_1(input)
 
 end
 
 # Returns an array of all invalid ids within a given range.
-# @param range [Array<String>, #read] an array with the start and end values of the range
+# @param range [Array<String>] an array with the start and end values of the range
 # @return [Array<Integer>] a collection of invalid ids (empty array if none)
 def find_invalid_ids(range)
   result = []
   a, b = range
+  # initialize start value with first invalid id
   if a.length.odd?
     a = next_invalid_id(a)
-    if a > b
-      return result
+  else
+    unless invalid?(a)
+      a = next_invalid_id(a)
     end
   end
+  # check if first invalid id is within the range
+  if a > b
+    return result
+  end
+  # find all invalid ids
+  while a <= b and invalid?(a)
+    result.push(a)
+    a = next_invalid_id(a)
+  end
+  # if invalid?(a)
+  #   result.push(a)
+
+  # end
 end
 
-# Determines the next invalid id given a valid id with odd length.
-# @param number [String, #read] the number as a string
-# @return [Integer] the next invalid id
-def next_invalid_id(number)
-  sequence = 10 ** ((number.length + 1) / 2 - 1)
-  sequence.to_s * 2
+# Determines the next invalid id.
+# @param number [String] the number as a string
+# @return [String] the next invalid id
+def next_invalid_id(id)
+  s = id.to_s
+  half =
+    if invalid?(id)
+      s[0...s.length/2].to_i
+    else
+      10 ** ((s.length - 1) / 2)
+    end
+  x = half + 1
+  return (x.to_s * 2).to_i
 end
 
 # Checks if an id is invalid
-# @param id [Integer, #read] the provided id
+# @param id [Integer] the provided id
 # @return [Boolean]
 def invalid?(id)
-  return false if id.to_s.length.odd?
+  s = id.to_s
+  return false if s.length.odd?
+  half = s.length / 2
+  return s[0...half] == s[half..-1]
 end
+
+# def part_2(input)
+# end
 
 solve!("Part 1:", part_1(RANGES))
-
-def part_2(input)
-end
-
-solve!("Part 2:", part_2(RANGES))
+# solve!("Part 2:", part_2(RANGES))
