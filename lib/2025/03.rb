c5b34def03555c537cd56d8d03361a716c5ab20e
Katie Lin <katielin019@gmail.com>
2025-12-17 15:47:23 -0800

    add logic, refactor in progress

--- lib/2025/03.rb
+++ lib/2025/03.rb
@@ -1,14 +1,26 @@
 LINES = INPUT.split("\n")
 
 def part_1(input)
-  # find the largest value in bank[0...-1]
-  # find the largest value from bank[index..-1]
-
   # transform string of numbers to an array of numbers
-  batteries.reduce do |memo, battery|
+
+  # # find the largest value in bank[0...-1]
+  first = batteries.reduce do |memo, battery|
     battery > memo ? battery : memo
   end
   # find the first index of battery value and then search the remaining list for the next highest value
+  first_idx = batteries.index(first)
+  remaining = batteries[first_idx + 1..-1]
+  # find the largest value from bank[index + 1..-1]
+  second = remaining.reduce do |memo, battery|
+    battery > memo ? battery : memo
+  end
+  second_idx = remaining.index(second) + first_idx + 1
+end
+
+def largest_joltage(bank)
+  first = bank.reduce do |memo, battery|
+    battery > memo ? battery : memo
+  end
 
 end
 
