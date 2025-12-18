c86ba0a2d3cc64a3bbf8688751a5b0b24e399249
Katie Lin <katielin019@gmail.com>
2025-12-17 16:09:12 -0800

    ok i was just using reduce wrong

--- lib/2025/03.rb
+++ lib/2025/03.rb
@@ -1,19 +1,17 @@
 BANKS = INPUT.split("\n")
 
 def part_1(input)
-  # transform string of numbers to an array of numbers
-  input.reduce do |total, bank|
-    bank.chars.map(&:to_i)
+  # banks = input.map { |line| line.chars.map(&:to_i) }
+  # banks = input.map(&:to_i).digits.reverse
+  banks.reduce(0) do |total, bank|
+    total + largest_joltage(bank)
   end
-  # call largest_joltage
-  # sum total output joltage
 end
 
 def largest_joltage(bank)
   first = max(bank[0...-1])
   partition = bank.index(first) + 1
-  remaining = bank[partition..-1]
-  second = max(remaining)
+  second = max(bank[partition..-1])
   first * 10 + second
 end
 
@@ -23,9 +21,9 @@ def max(batteries)
   end
 end
 
-solve!("Part 1:", part_1(LINES))
+solve!("Part 1:", part_1(BANKS))
 
 def part_2(input)
 end
 
-solve!("Part 2:", part_2(LINES))
+solve!("Part 2:", part_2(BANKS))
